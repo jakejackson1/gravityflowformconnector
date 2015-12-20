27b59f19dd925a80743a980a8f8ff8bb55841385
Steve Henty <stevehenty@gmail.com>
2015-12-20 23:01:14 +0100

    support multi-input field mapping

--- includes/class-step-form-connector.php
+++ includes/class-step-form-connector.php
@@ -217,9 +217,15 @@ if ( class_exists( 'Gravity_Flow_Step' ) ) {
 
 			$remote_form = $this->get_target_form( $target_form_id );
 
+
+
 			if ( empty( $remote_form) ) {
 				return false;
 			}
+
+			$fields = $this->get_field_map_choices( $remote_form );
+			return $fields;
+
 			$remote_fields = $remote_form['fields'];
 
 			foreach ( $remote_fields as $field ) {
@@ -269,6 +275,8 @@ if ( class_exists( 'Gravity_Flow_Step' ) ) {
 			$api = new Gravity_Flow_API( $this->target_form_id );
 			$steps = $api->get_steps();
 
+			$form = $this->get_form();
+
 			if ( is_array( $this->mappings ) ) {
 				foreach ( $this->mappings as $mapping ) {
 
@@ -277,9 +285,23 @@ if ( class_exists( 'Gravity_Flow_Step' ) ) {
 					}
 
 					$target_field_id = trim( $mapping['key'] );
-					$external_field_id = $mapping['value'];
-					$new_entry[ $target_field_id ] = $entry[ $external_field_id ];
+					$source_field_id = $mapping['value'];
 
+					if ( $source_field_id == intval( $source_field_id ) ) {
+						$source_field = GFFormsModel::get_field( $form, $source_field_id );
+						$inputs = $source_field->get_entry_inputs();
+						if ( is_array( $inputs ) ) {
+							foreach( $inputs as $input ) {
+								$input_id = str_replace( $source_field_id, $target_field_id, $input['id'] );
+								$new_entry[ $input_id ] = $entry[ $input['id'] ];
+							}
+						} else {
+							$new_entry[ $target_field_id ] = $entry[ $source_field_id ];
+						}
+
+					} else {
+						$new_entry[ $target_field_id ] = $entry[ $source_field_id ];
+					}
 				}
 			}
 
@@ -349,6 +371,8 @@ if ( class_exists( 'Gravity_Flow_Step' ) ) {
 				'form_id' => $this->target_form_id,
 			);
 
+			$form = $this->get_form();
+
 			if ( is_array( $this->mappings ) ) {
 				foreach ( $this->mappings as $mapping ) {
 
@@ -357,8 +381,23 @@ if ( class_exists( 'Gravity_Flow_Step' ) ) {
 					}
 
 					$target_field_id = trim( $mapping['key'] );
-					$external_field_id = $mapping['value'];
-					$new_entry[ $target_field_id ] = $entry[ $external_field_id ];
+					$source_field_id = $mapping['value'];
+
+					if ( $source_field_id == intval( $source_field_id ) ) {
+						$source_field = GFFormsModel::get_field( $form, $source_field_id );
+						$inputs = $source_field->get_entry_inputs();
+						if ( is_array( $inputs ) ) {
+							foreach( $inputs as $input ) {
+								$input_id = str_replace( $source_field_id, $target_field_id, $input['id'] );
+								$new_entry[ $input_id ] = $entry[ $input['id'] ];
+							}
+						} else {
+							$new_entry[ $target_field_id ] = $entry[ $source_field_id ];
+						}
+
+					} else {
+						$new_entry[ $target_field_id ] = $entry[ $source_field_id ];
+					}
 
 				}
 			}
@@ -546,6 +585,115 @@ if ( class_exists( 'Gravity_Flow_Step' ) ) {
 				$choices[] = array( 'label' => $display_name, 'value' => $assignee_key );
 			}
 			return $choices;
+		}
+
+		public function get_field_map_choices( $form, $field_type = null, $exclude_field_types = null ) {
+
+			$fields = array();
+
+			// Setup first choice
+			if ( rgblank( $field_type ) || ( is_array( $field_type ) && count( $field_type ) > 1 ) ) {
+
+				$first_choice_label = __( 'Select a Field', 'gravityforms' );
+
+			} else {
+
+				$type = is_array( $field_type ) ? $field_type[0] : $field_type;
+				$type = ucfirst( GF_Fields::get( $type )->get_form_editor_field_title() );
+
+				$first_choice_label = sprintf( __( 'Select a %s Field', 'gravityforms' ), $type );
+
+			}
+
+			$fields[] = array( 'value' => '', 'label' => $first_choice_label );
+
+			// if field types not restricted add the default fields and entry meta
+			if ( is_null( $field_type ) ) {
+				$fields[] = array( 'value' => 'id', 'label' => esc_html__( 'Entry ID', 'gravityforms' ) );
+				$fields[] = array( 'value' => 'date_created', 'label' => esc_html__( 'Entry Date', 'gravityforms' ) );
+				$fields[] = array( 'value' => 'ip', 'label' => esc_html__( 'User IP', 'gravityforms' ) );
+				$fields[] = array( 'value' => 'source_url', 'label' => esc_html__( 'Source Url', 'gravityforms' ) );
+
+				$server_type = $this->get_setting( 'server_type' );
+				$entry_meta = $server_type == 'remote' ? array() : GFFormsModel::get_entry_meta( $form['id'] );
+				foreach ( $entry_meta as $meta_key => $meta ) {
+					$fields[] = array( 'value' => $meta_key, 'label' => rgars( $entry_meta, "{$meta_key}/label" ) );
+				}
+			}
+
+			// Populate form fields
+			if ( is_array( $form['fields'] ) ) {
+				foreach ( $form['fields'] as $field ) {
+					$input_type = $field->get_input_type();
+					$inputs     = $field->get_entry_inputs();
+					$field_is_valid_type = ( empty( $field_type ) || ( is_array( $field_type ) && in_array( $input_type, $field_type ) ) || ( ! empty( $field_type ) && $input_type == $field_type ) );
+
+					if ( is_null( $exclude_field_types ) ) {
+						$exclude_field = false;
+					} elseif ( is_array( $exclude_field_types ) ) {
+						if ( in_array( $input_type, $exclude_field_types ) ) {
+							$exclude_field = true;
+						} else {
+							$exclude_field = false;
+						}
+					} else {
+						//not array, so should be single string
+						if ( $input_type == $exclude_field_types ) {
+							$exclude_field = true;
+						} else {
+							$exclude_field = false;
+						}
+					}
+
+					if ( is_array( $inputs ) && $field_is_valid_type && ! $exclude_field ) {
+						//If this is an address field, add full name to the list
+						if ( $input_type == 'address' ) {
+							$fields[] = array(
+								'value' => $field->id,
+								'label' => GFCommon::get_label( $field ) . ' (' . esc_html__( 'Full', 'gravityforms' ) . ')'
+							);
+						}
+						//If this is a name field, add full name to the list
+						if ( $input_type == 'name' ) {
+							$fields[] = array(
+								'value' => $field->id,
+								'label' => GFCommon::get_label( $field ) . ' (' . esc_html__( 'Full', 'gravityforms' ) . ')'
+							);
+						}
+						//If this is a checkbox field, add to the list
+						if ( $input_type == 'checkbox' ) {
+							$fields[] = array(
+								'value' => $field->id,
+								'label' => GFCommon::get_label( $field ) . ' (' . esc_html__( 'Selected', 'gravityforms' ) . ')'
+							);
+						}
+
+						foreach ( $inputs as $input ) {
+							$fields[] = array(
+								'value' => $input['id'],
+								'label' => GFCommon::get_label( $field, $input['id'] )
+							);
+						}
+					} elseif ( $input_type == 'list' && $field->enableColumns && $field_is_valid_type && ! $exclude_field ) {
+						$fields[] = array(
+							'value' => $field->id,
+							'label' => GFCommon::get_label( $field ) . ' (' . esc_html__( 'Full', 'gravityforms' ) . ')'
+						);
+						$col_index = 0;
+						foreach ( $field->choices as $column ) {
+							$fields[] = array(
+								'value' => $field->id . '.' . $col_index,
+								'label' => GFCommon::get_label( $field ) . ' (' . esc_html( rgar( $column, 'text' ) ) . ')',
+							);
+							$col_index ++;
+						}
+					} elseif ( ! rgar( $field, 'displayOnly' ) && $field_is_valid_type && ! $exclude_field ) {
+						$fields[] = array( 'value' => $field->id, 'label' => GFCommon::get_label( $field ) );
+					}
+				}
+			}
+
+			return $fields;
 		}
 
 	}
